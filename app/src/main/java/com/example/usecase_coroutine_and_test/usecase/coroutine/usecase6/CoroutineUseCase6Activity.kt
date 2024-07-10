2357e6bedebb6c77f1145851f25f12d1583a7bc7
Anna <wenan@staff.1111.com.tw>
2024-07-10 17:38:03 +0800

    新增 Use Case 6 Perform network requests timeout or retry

--- app/src/main/java/com/example/usecase_coroutine_and_test/usecase/coroutine/usecase6/CoroutineUseCase6Activity.kt
+++ app/src/main/java/com/example/usecase_coroutine_and_test/usecase/coroutine/usecase6/CoroutineUseCase6Activity.kt
@@ -1,12 +1,13 @@
 package com.example.usecase_coroutine_and_test.usecase.coroutine.usecase6
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.usecase_coroutine_and_test.R
-import com.example.usecase_coroutine_and_test.databinding.ActivityCoroutineUseCaseBinding
+import androidx.core.view.isVisible
+import coil.load
+import com.example.usecase_coroutine_and_test.constant.UiState
+import com.example.usecase_coroutine_and_test.databinding.ActivityCoroutineUseCaseImageBinding
+import com.example.usecase_coroutine_and_test.utils.toast
 
 
 /**
@@ -14,12 +15,32 @@ import com.example.usecase_coroutine_and_test.databinding.ActivityCoroutineUseCa
  */
 class CoroutineUseCase6Activity : AppCompatActivity() {
 
-    private val binding by lazy { ActivityCoroutineUseCaseBinding.inflate(layoutInflater) }
+    private val binding by lazy { ActivityCoroutineUseCaseImageBinding.inflate(layoutInflater) }
+    private val viewModel: CoroutineUseCase6VIewModel by viewModels()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        viewModel.networkPerformRetryAndTimeout()
+
+        viewModel.uiState().observe(this) { uiState ->
+            when (uiState) {
+                is UiState.Loading -> binding.progressBar.isVisible = true
+                is UiState.Success -> binding.progressBar.isVisible = false
+                is UiState.Error -> {
+                    binding.progressBar.isVisible = false
+                    toast(uiState.errorMsg)
+                }
+            }
+        }
+
+        viewModel.pokemonInfoList.observe(this) { infoList ->
+            binding.tvName.text = infoList.first().name
+            binding.imageView.load(infoList.first().imageUrl)
+            binding.tvName2.text = infoList.last().name
+            binding.imageView2.load(infoList.last().imageUrl)
+        }
     }
 }
